d98a30de1d0465ed531ebebf3d2188a6332e8c83
Sebastian Egbers <sebastian@egbers.info>
2018-12-27 11:24:50 +0100

    added hotkeys.

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -17,6 +17,11 @@
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-12 text-center">
+        HotKeys: <span class="badge badge-warning">KeyUp</span> + <span class="badge badge-warning">KeyDown</span> + <span class="badge badge-warning">+</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,30 +40,58 @@ export default {
     notes: Notes,
     index: 0
   }),
+  mounted() {
+    // install key listener for hotskeys
+    var vm = this;
+    window.addEventListener('keyup', function(event) {
+      // Check for elements, that do not listen to our hot keys
+      if (["INPUT", "TEXTAREA"].indexOf(document.activeElement.tagName) >= 0 ) {
+        return;
+      }
+      // check key down
+      if (event.keyCode == 40) {
+        vm.index++;
+        if (vm.index >= vm.notes.list.length) {
+          vm.index = vm.notes.list.length - 1;
+        }
+      }
+      // check key up
+      if (event.keyCode == 38) {
+        vm.index--;
+        if (vm.index < 0) {
+          vm.index = 0;
+        }
+      }
+      // check for +
+      if (event.keyCode == 187) {
+        vm.newNote();
+      }
+    });
+  },
+  beforeMount() {
+    this.notes.load();
+  },
   methods: {
     newNote() {
       this.notes.list.push({
         title: '',
         note: '',
         dirty: true,
-      })
-      this.index = this.notes.list.length - 1
+      });
+      this.index = this.notes.list.length - 1;
     },
     changeNote(index) {
-      this.index = index
+      this.index = index;
     },
     saveNote() {
-      this.notes.update(this.index)
+      this.notes.update(this.index);
     },
     deleteNote() {
-      this.notes.delete(this.index)
+      this.notes.delete(this.index);
       if (this.index > this.notes.list.length) {
-        this.index = this.notes.list.length
+        this.index = this.notes.list.length;
       }
     }
-  },
-  beforeMount() {
-    this.notes.load();
   }
 }
 </script>
