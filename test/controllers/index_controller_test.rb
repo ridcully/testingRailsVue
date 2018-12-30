02ea5057086055391cc468632a8e9f4820351243
Sebastian Egbers <sebastian@egbers.info>
2018-12-30 15:45:03 +0100

    fixed autogenerated tests.

--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class IndexControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get index_index_url
+    get '/'
     assert_response :success
   end
 
